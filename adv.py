fe18ae23631afb7e246bbd93ca3be62a08f5ecc6
Eli Sacks <bastlifa@gmail.com>
2020-01-23 14:40:38 -0500

    util func, some graph work

--- adv.py
+++ adv.py
@@ -13,8 +13,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,7 +29,33 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# util
+def reverse_dir(direction):
+        rev_dict = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+        return rev_dict[direction]
 
+# Graph
+class Graph():
+    def __init__(self):
+        self.rooms = {}
+    
+    
+
+    def add_room(self, room):
+        self.rooms[room] = {'n': '?', 'e': '?', 's': '?', 'w': '?'}
+    
+    def add_edge(self, room_1, room_2, dir_1_to_2):
+        if room_1 in self.rooms and room_2 in self.rooms:
+            self.rooms[room_1][dir_1_to_2] = room_2
+            self.rooms[room_2][reverse_dir(dir_1_to_2)] = room_1
+        else: raise IndexError("ERROR! That room does not exist yet!")
+
+
+
+
+# find_path_to_closest_unexplored
+def fptcu():
+    pass
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +77,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
